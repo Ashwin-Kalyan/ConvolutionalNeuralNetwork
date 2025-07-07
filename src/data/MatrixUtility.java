0306323e790576a23bdfa5d000b0542e3a8f7ee8
Ashwin Kalyan <64381851+Ashwin-Kalyan@users.noreply.github.com>
2025-07-07 17:44:40 -0500

    MatrixUtility
    Add
    Multiply
    
    Supports 1D and 2D matrices

---
+++ src/data/MatrixUtility.java
@@ -0,0 +1,19 @@
+package ConvolutionalNeuralNetwork.ConvolutionalNeuralNetwork.src.data;
+
+public class MatrixUtility {
+    public static double[][] add(double[][] a, double[][] b) {        
+        double[][] out = new double[a.length][a[0].length];
+        
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                out[i][j] = a[i][j] + b[i][j];
+            }
+        }
+        
+        return out;
+    }
+
+    public static double[] add(double[] a, double[] b) {        
+        double[] out = new double[a.length];
+        
+        for (int i = 0; i < a.length; i++) {
